c8d8fed96e1da1a206d6949b8e84ac7feadc5c7d
stetaf <s.tafuri95@gmail.com>
2021-08-03 15:23:20 +0200

    Create and import header component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
+    <Header/>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
+import Header from './components/Header.vue'
 import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Header
   }
 }
 </script>
---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <div class="container">
+      <div class="row">
+        <img src="https://fontmeme.com/temporary/4326ebd4aaad8208a906a6850f07aa1c.png" alt="">
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped lang="scss">
+header {
+  background-color: #000000;
+  padding: 10px 30px;
+  text-align: left;
+}
+</style>
