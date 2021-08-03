b42739026ba0f0fe4a6441c10e47e50a48079461
stetaf <s.tafuri95@gmail.com>
2021-08-03 15:32:06 +0200

    Add call to themoviedb to get marvel movies

---
+++ src/components/Main.vue
@@ -0,0 +1,38 @@
+<template>
+  <main>
+    <div class="container">
+      <div class="row">
+
+      </div>
+    </div>
+  </main>
+</template>
+
+<script>
+  const axios = require('axios').default;
+
+  export default {
+    name: 'Main',
+    props: {
+      msg: String
+    },
+    data() {
+      return {
+        movies: null
+      }
+    },
+    mounted() {
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=5eb5e38903b62b2d5616df76724f5b67&language=en-US&page=1&include_adult=false&query=marvel')
+           .then(resp => {
+             this.movies = resp.data.results;
+           })
+           .catch(e => {
+             console.log('Sorry, something went wrong! Error: ' + e);
+           });
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
