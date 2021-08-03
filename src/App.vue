8f7bf9f8433ca0ff02933af08f6562e2353ce2b2
stetaf <s.tafuri95@gmail.com>
2021-08-03 15:31:51 +0200

    Rename default component to main component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,19 @@
 <template>
   <div id="app">
     <Header/>
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Main msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
-import HelloWorld from './components/HelloWorld.vue'
+import Main from './components/Main.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    Header
+    Header,
+    Main
   }
 }
 </script>
