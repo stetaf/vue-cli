a32354567d912ffde9bbed8e2892f6d937dc67f9
stetaf <s.tafuri95@gmail.com>
2021-08-04 15:01:49 +0200

    Add login and register templates

--- src/components/core/auth/Login.vue
+++ src/components/core/auth/Login.vue
@@ -1,5 +1,28 @@
 <template>
-    <h1>Login</h1>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-5">
+                <div class="card mt-4">
+                    <div class="card-header">
+                        <h1>Login</h1>
+                    </div>
+                    <div class="card-body">
+                        <form>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Email address</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Password</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
 </template>
 
 <script>
--- src/components/core/auth/Register.vue
+++ src/components/core/auth/Register.vue
@@ -1,5 +1,32 @@
 <template>
-    <h1>Register</h1>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-5">
+                <div class="card mt-4">
+                    <div class="card-header">
+                        <h1>Register</h1>
+                    </div>
+                    <div class="card-body">
+                        <form>
+                            <div class="form-group">
+                                <label for="exampleInputNmail1">Full name</label>
+                                <input type="name" class="form-control" id="exampleInputName1" aria-describedby="nameHelp" placeholder="Enter your full name">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputEmail1">Email address</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+                            </div>
+                            <div class="form-group">
+                                <label for="exampleInputPassword1">Password</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>    
 </template>
 
 <script>
