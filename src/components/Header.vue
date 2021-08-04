807c2c2fed75ebf784dc0d16cb4c772437444955
stetaf <s.tafuri95@gmail.com>
2021-08-04 14:48:29 +0200

    Add nav for login and register routes

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,12 @@
 <template>
   <header>
-    <img src="./../assets/logo.png" alt="">
+    <div class="logo">
+      <img src="./../assets/logo.png" alt="">
+    </div>
+    <div class="nav">
+      <router-link to="/login">Login</router-link>
+      <router-link to="/register">Register</router-link>
+    </div>
   </header>
 </template>
 
@@ -12,8 +18,20 @@ export default {
 
 <style scoped lang="scss">
 header {
+  display: flex;
+  justify-content: space-between;
   background-color: #000000;
   padding: 10px 30px;
   text-align: left;
+  .nav {
+    display: flex;
+    align-items: center;
+    
+    a { 
+      font-size: 18px;
+      color: #dc1a28;
+      margin-left: 15px;
+    }
+  }
 }
 </style>
