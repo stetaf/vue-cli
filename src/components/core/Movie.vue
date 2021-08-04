25df7c836209c9cafb7f327de3e802badbdf0411
stetaf <s.tafuri95@gmail.com>
2021-08-04 14:36:38 +0200

    Change movies to movie

---
+++ src/components/core/Movie.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="col-3">
+      <div :class="[(num < 4) ? 'card mb-3 mt-3' : 'card mb-3 ']">
+        <img :src="'https://image.tmdb.org/t/p/w342' + movie.poster_path" :alt="movie.original_title + ' cover'">
+      </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Movies',
+    props: {
+      movie: Object,
+      num: Number
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
