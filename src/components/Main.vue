e628378338910182d12a1023fddf18b92e9a58c8
stetaf <s.tafuri95@gmail.com>
2021-08-04 14:37:17 +0200

    Change how movies are displayed

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,18 +1,36 @@
 <template>
   <main>
     <div class="container">
-      <Movies />
+      <div class="row">
+        <Movie v-for="(movie, num) in movies" :key="movie.id" :movie="movie" :num="num"/>
+      </div>
     </div>
   </main>
 </template>
 
 <script>
-  import Movies from './core/Movies.vue'
+  import Movie from './core/Movie.vue'
+
+  const axios = require('axios').default;
 
   export default {
     name: 'Main',
     components: {
-      Movies
+      Movie
+    },
+    data() {
+      return {
+        movies: null
+      }
+    },
+    mounted() {
+      axios.get('https://api.themoviedb.org/3/search/movie?api_key=5eb5e38903b62b2d5616df76724f5b67&language=en-US&page=1&include_adult=false&query=marvel')
+            .then(resp => {
+              this.movies = resp.data.results;
+            })
+            .catch(e => {
+              console.log('Sorry, something went wrong! Error: ' + e);
+            });
     }
   }
 </script>
