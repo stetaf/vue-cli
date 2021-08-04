1cb3f984cd23eb1cde1acde736be523e5e69049c
stetaf <s.tafuri95@gmail.com>
2021-08-04 14:53:30 +0200

    Remove main from included components

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,17 @@
 <template>
   <div id="app">
     <Header/>
-    <Main msg="Welcome to Your Vue.js App"/>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue'
-import Main from './components/Main.vue'
 
 export default {
   name: 'App',
   components: {
-    Header,
-    Main
+    Header
   }
 }
 </script>
