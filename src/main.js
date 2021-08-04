bd8b713cc7a7c839e9352209c6fe394fd6f3ccca
stetaf <s.tafuri95@gmail.com>
2021-08-04 14:46:46 +0200

    Create login and register routes and components

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const Log = () => import('./components/core/auth/Login.vue')
+const Reg = () => import('./components/core/auth/Register.vue')
+
+const routes = [
+  { path: '/login', component: Log },
+  { path: '/register', component: Reg }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  routes
+})
 
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
